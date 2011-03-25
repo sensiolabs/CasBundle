be43a3f8b8234124c2381c3dbc28c22a0131b111
Jean-François Simon <contact@jfsimon.fr>
2011-03-25 08:44:17 +0100

    move config to new system

--- DependencyInjection/SensioCasExtension.php
+++ DependencyInjection/SensioCasExtension.php
@@ -6,32 +6,26 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Definition\Processor;
 
 class SensioCasExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('cas.xml');
+        $processor = new Processor();
+        $configuration = new Configuration();
 
-        $config = array();
-        foreach ($configs as $conf) {
-            $config = array_merge($config, $conf);
-        }
+        // add configuration
+        $tree = $configuration->getConfigTree();
+        $config = $processor->process($tree, $configs);
 
         foreach ($config as $key => $value) {
-            $container->setParameter('cas.'.$key, $value);
+            $container->setParameter('sensio_cas.'.$key, $value);
         }
-    }
 
-    public function getXsdValidationBasePath()
-    {
-        return __DIR__.'/../Resources/config/schema';
-    }
-
-    public function getNamespace()
-    {
-        return null;
+        // load service
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('cas.xml');
     }
 
     public function getAlias()
