3d9850b29099a947e4d2c7ec6a9607f9276b4164
Jean-François Simon <contact@jfsimon.fr>
2011-03-25 07:38:20 +0100

    change DIC namespaces

--- DependencyInjection/SensioCasExtension.php
+++ DependencyInjection/SensioCasExtension.php
@@ -2,10 +2,10 @@
 
 namespace Sensio\CasBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
 
 class SensioCasExtension extends Extension
 {
