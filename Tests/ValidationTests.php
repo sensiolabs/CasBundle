4cee7f3b0db282434ee8780ef4216ac06d2b7876
Christophe Coevoet <stof@notk.org>
2011-06-19 23:27:53 +0200

    Updated the testcases

--- Tests/ValidationTests.php
+++ Tests/ValidationTests.php
@@ -8,14 +8,14 @@ class ValidationTest extends WebTestCase
 {
     public function testSuccess()
     {
-        $client = $this->createClient();
+        $client = self::createClient();
         $crawler = $client->request('GET', '/cas/protected?ticket=success');
         $this->assertEquals('access granted', $crawler->text());
     }
 
     public function testError()
     {
-        $client = $this->createClient();
+        $client = self::createClient();
         $crawler = $client->request('GET', '/cas/protected?ticket=error');
         $this->assertTrue($client->getResponse()->isRedirection());
     }
